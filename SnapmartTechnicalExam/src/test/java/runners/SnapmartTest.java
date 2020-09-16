e5fd252f0037f2820d425f4f553c769192fbbd02
evanrbriones <evan.briones@haelthtech.com>
2020-09-16 17:23:22 +0800

    used the built-in html report generator

--- SnapmartTechnicalExam/src/test/java/runners/SnapmartTest.java
+++ SnapmartTechnicalExam/src/test/java/runners/SnapmartTest.java
@@ -1,6 +1,6 @@
 package runners;
-import org.junit.runner.RunWith;
 
+import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
@@ -9,11 +9,11 @@ import cucumber.api.junit.Cucumber;
 		features ="src/test/resources/functionalTests",
 		glue= {"stepDefinitions"},
 		monochrome = true,
-		plugin = {"json:target/cucumber-report/cucumber.json"},
+		plugin = { "pretty", "html:target/cucumber-reports" },
 		junit = "--step-notifications"
 		
 )
 
 public class SnapmartTest {
-
+	
 }
